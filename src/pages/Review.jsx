4bed81b598bc9f25acd84593ef28db18a62e9df6
Marko Zdravkovski <marko@intervaro.se>
2022-01-16 22:31:51 +0100

    Bugfix in review page

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { useParams } from 'react-router'
 import usePhotos  from "../hooks/usePhotos"
 import PhotosGrid from "../components/PhotosGrid"
@@ -15,7 +15,7 @@ const Review = () => {
     const [reviewSend, setReviewSend] = useState(null)
     const create = useCreateAlbum()
     const album = useAlbum(id)
-    const { notChosenPhotos, chosenPhotos} = usePhotoContext()
+    const { notChosenPhotos, chosenPhotos } = usePhotoContext()
 
     const handleReview = (e) => {
         e.preventDefault()
@@ -34,20 +34,23 @@ const Review = () => {
     return (
         <div>
             { !reviewSend && <>
-            { photos.data && <PhotosGrid query={photos}  review={true}/> }
+            { photos.data && <PhotosGrid query={ photos }  review={ true }/> }
            <form onSubmit={ handleReview }>
             <label htmlFor="name">Your name:</label>
             <input type="text" name="name" ref={ nameRef } required />
             <Button type="submit">Send review</Button>
            </form>
-           <div className="summary">
-            <p>Accepted photos:</p>
-            {chosenPhotos.length} / {photos.data.length}
-           </div>
-           <div className="summary">
-            <p>Rejected photos:</p>
-            {notChosenPhotos.length} / {photos.data.length}
-           </div>
+            { photos.data &&
+            <> 
+                <div className="summary">
+                    <p>Accepted photos:</p>
+                    { chosenPhotos.length } / { photos.data.length }
+                </div>
+                <div className="summary">
+                    <p>Rejected photos:</p>
+                    { notChosenPhotos.length } / { photos.data.length }
+                </div>
+            </> }
            { message ? <p style={{marginTop: "2rem"}}>You need to approve or reject every photo!</p> : null }
            </>}
            { reviewSend && <div className="review-submited">
